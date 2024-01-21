1d4407d8c9400c19b7250daabdb10e21c1d8670e
Ram Deshmukh <114208322+ramdeshmukh3003@users.noreply.github.com>
2024-01-21 08:42:10 +0530

    Update Star_Pattern#12.cpp

--- Star_Pattern#12.cpp
+++ Star_Pattern#12.cpp
@@ -4,11 +4,11 @@ int main() {
     // Write C++ code here
     int n;
     cin>>n;
-    for(int row=0;row<n;row++){
-        for(int space=0;space<n-1-row;space++){
+    for(int rows=0;rows<n;rows++){
+        for(int spaces=0;spaces<n-1-rows;spaces++){
             cout<<" ";
         }
-        for(int col=0;col<row+1;col++){
+        for(int col=0;col<rows+1;col++){
             cout<<"*";
         }
         cout<<endl;
