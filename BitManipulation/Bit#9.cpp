9b1daa0e97302ec6427b5a2e1a82bed769f19b76
Ram Deshmukh <114208322+ramdeshmukh3003@users.noreply.github.com>
2024-01-25 00:23:48 +0530

    Update Bit#9.cpp

--- BitManipulation/Bit#9.cpp
+++ BitManipulation/Bit#9.cpp
@@ -1,3 +1,12 @@
+// Q. Toggle kth bit of integer n?
+
+// Input: n=8 k=2
+// Output: 12
+// Explanation: the binary representation of 8 is 1000 . after toggling the 3rd bit the number becomes 12.
+
+// Code:-
+
+
 #include<bits/stdc++.h>>
 using namespace std;
 
