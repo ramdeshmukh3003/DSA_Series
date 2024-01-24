dc972135891a199ad3e42aca8276c26a25b7fbb2
Ram Deshmukh <114208322+ramdeshmukh3003@users.noreply.github.com>
2024-01-24 22:56:49 +0530

    Update Bit#11.cpp

--- BitManipulation/Bit#11.cpp
+++ BitManipulation/Bit#11.cpp
@@ -1,3 +1,16 @@
+// Q. Swap the two numbers without using temp variable and arithmatic operation ?
+
+// Input: n=8 , k=7
+// Output: n=7 , k=8
+// Explanation: the binary representation of n=8 is 1000 and k=7 is 0111.
+   Do this 3 operations -
+    n=n^k;
+    k=n^k;
+    n=n^k;
+   after doing all above operation in same manner the nyou will get expected output.
+
+// code:
+
 #include<bits/stdc++.h>>
 using namespace std;
 
