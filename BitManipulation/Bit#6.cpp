ebdeaa58b7505cd01f0e8b0aa934d6c934f838b6
Ram Deshmukh <114208322+ramdeshmukh3003@users.noreply.github.com>
2024-01-25 00:01:47 +0530

    Update Bit#6.cpp

--- BitManipulation/Bit#6.cpp
+++ BitManipulation/Bit#6.cpp
@@ -1,3 +1,11 @@
+// Q. finding ith bit in the binary representation of n?
+
+// Input: n=15 i=3
+// Output: 1
+// Explanation: in the binary representation of 15 then index 3 bit is 1.
+
+// Code:-
+
 #include <iostream>
 using namespace std;
 
@@ -6,7 +14,7 @@ int main()
     int x,i;
     cin>>x>>i;
     int mask = (x & (1<<(i-1)));
-    int ans = mask << (i-1);
+    int ans = mask >> (i-1);
     cout<<ans<<endl;
     return 0;
 }
