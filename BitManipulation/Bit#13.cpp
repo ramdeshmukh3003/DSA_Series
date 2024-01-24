19bea72ccdec780f71f450ae6b13f263f19ecce8
Ram Deshmukh <114208322+ramdeshmukh3003@users.noreply.github.com>
2024-01-24 23:15:58 +0530

    Update Bit#13.cpp

--- BitManipulation/Bit#13.cpp
+++ BitManipulation/Bit#13.cpp
@@ -1,3 +1,11 @@
+// Q. Check the integer n has bits in an alternate pattern  ?
+
+// Input: n=10
+// Output: Yes
+// Explanation: the binary representation of 10 is 1010 then the bits are in alternate pattern so the answer is yes.
+
+// Code:-
+
 #include <bits/stdc++.h>  
 using namespace std; 
 
